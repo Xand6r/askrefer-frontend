7ae65e998f9e87ebeb0c823b1308e879029ba281
xander <shuaibualexander@gmail.com>
2022-02-11 22:57:58 +0100

    fix tagline

--- src/pages/landing/index.jsx
+++ src/pages/landing/index.jsx
@@ -1,40 +1,45 @@
 import { useHistory } from "react-router-dom";
-import Slider from "react-slick";
+// import Slider from "react-slick";
 import Megaphone from "@/assets/svgs/landingicon.svg";
 import Button from "@/components/button";
-import { QuestionOne, QuestionTwo, QuestionThree } from "./components/questions";
+// import { QuestionOne, QuestionTwo, QuestionThree } from "./components/questions";
 import "./styles.scss";
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
-const CTA_TEXT = "Get Started";
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
+const CTA_TEXT = "Create an Ask";
 
 export default function Index() {
-    const history = useHistory();
-    const settings = {
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        arrows: false
-      };
-    /**
-     * The CTA to be called when we choose to go to the next page
-     */
-    const onClickButton = () => {
-        history.push('/post');
-    };
+  const history = useHistory();
+  // const settings = {
+  //     infinite: true,
+  //     speed: 500,
+  //     slidesToShow: 1,
+  //     slidesToScroll: 1,
+  //     autoplay: true,
+  //     arrows: false
+  //   };
+  /**
+   * The CTA to be called when we choose to go to the next page
+   */
+  const onClickButton = () => {
+    history.push("/post");
+  };
 
-    return (
-        <div id="landing-page">
-            <section className="header">
-                <img src={Megaphone} alt="" />
-                <h4>
-                    Sometimes you need a personal touch
-                </h4>
-            </section>
+  return (
+    <div id="landing-page">
+      <section className="header">
+        <img src={Megaphone} alt="" />
+        <div>
+          <h2>Make your network work for you</h2>
+          <h4>
+            Secure, sharable links to help your network refer your next
+            investor, service provider or team member.
+          </h4>
+        </div>
+      </section>
 
-            <section className="questions">
+      {/* to be deleted eventually */}
+      {/* <section className="questions">
                 <Slider {...settings}>
                     <div>
                         <QuestionOne />
@@ -46,14 +51,13 @@ export default function Index() {
                         <QuestionThree />
                     </div>
                 </Slider>
-            </section>
+            </section> */}
+      {/* to be deleted eventually */}
 
-            <section className="cta">
-                <h5>
-                    AskRefer empowers your network to search for the right person
-                </h5>
-                <Button text={CTA_TEXT} onClick={onClickButton}/>
-            </section>
-        </div>
-    );
+      <section className="cta">
+        {/* <h5>AskRefer empowers your network to search for the right person</h5> */}
+        <Button text={CTA_TEXT} onClick={onClickButton} />
+      </section>
+    </div>
+  );
 }
